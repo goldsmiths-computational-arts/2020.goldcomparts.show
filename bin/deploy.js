7b84393c094d93fd6db7faf51dcc4eecf0a3a083
Julian Burgess <aubergene@gmail.com>
2020-08-04 14:22:26 +0100

    add gitlab url

--- bin/deploy.js
+++ bin/deploy.js
@@ -5,7 +5,7 @@ ghpages.publish(
   "public", // <-- replace yourproject with your repo name
   {
     branch: "gh-pages",
-    repo: "https://github.com/aubergene/pop-up-show-may-2020.git",
+    repo: "https://gitlab.doc.gold.ac.uk/jburg002/2020-final-show.git",
     user: {
       name: "Julian Burgess",
       email: "aubergene@gmail.com",
