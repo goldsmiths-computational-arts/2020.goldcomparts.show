7df53985f8ccd998ab899e5daa89bfbd1cd3fad6
Julian Burgess <aubergene@gmail.com>
2020-08-12 11:21:17 +0100

    Updates

---
+++ client/helpers.ce81ccb7.js
@@ -0,0 +1,1 @@
+const e=e=>e.toLowerCase().replace(/[^a-z]+/g," ").trim().replace(/ /g,"-");function a(e,a,t){ga("send","event",e,a,t)}export{e as s,a as t};
---
+++ client/legacy/helpers.38afc14c.js
@@ -0,0 +1,1 @@
+var e=function(e){return e.toLowerCase().replace(/[^a-z]+/g," ").trim().replace(/ /g,"-")};function a(e,a,r){ga("send","event",e,a,r)}export{e as s,a as t};
