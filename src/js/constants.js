4b63bd27e7f4d16a405f299d34bee24d8a56b0a6
Julian Burgess <aubergene@gmail.com>
2020-09-19 10:51:49 +0100

    fix event url

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,6 +1,6 @@
 export const BASE_HOST = "2020.goldcomparts.show";
 export const BASE_URL = `https://${BASE_HOST}`;
-export const EVENT_URL = 
+export const EVENT_URL =
   "https://www.eventbrite.co.uk/e/chimera-garden-computational-arts-mamfa-degree-show-digital-exhibition-tickets-119608075975";
 export const REGISTER_URL =
-  "https://www.eventbrite.co.uk/e/chimera-garden-computational-arts-mamfa-degree-show-physical-exhibition-tickets-119513230289?aff=erelexpmlt";
+  "https://www.eventbrite.co.uk/e/chimera-garden-computational-arts-mamfa-degree-show-physical-exhibition-tickets-119513404811";
